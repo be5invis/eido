980bd7529423bac2a60b9a179f61ed7dfc250f62
Belleve Invis <infinte.cdda@hotmail.com>
2012-03-26 15:22:18 +0800

    npm ready.

--- bin/eidoc.js
+++ bin/eidoc.js
@@ -1,49 +1,28 @@
 var processor = require('../lib/processor');
-var gscope = require('../lib/scope').scope;
 var fork = processor.fork;
-var parse = processor.parse;
+var gscope = require('../lib/scope').scope;
 var path = require('path');
 var fs = require('fs')
 
 var scope = fork(gscope);
 
 // These functions are compilation-specific.
-scope.consoleOut = function(s){
-	console.log(s);
-	return s;
-};
-scope.fileOut = function(path, s){
-	fs.writeFileSync(path, s, 'utf-8');
-	return s;
-};
-scope.input = function(file){
-	var ast = parseFile(file);
-	this.__transform(ast);
-	return '';
-};
 scope.dirName = path.dirname;
-scope.execPath = process.argv[1];
+scope.execPath = path.resolve(process.argv[1]);
 scope.inputPath = path.normalize(process.argv[2]);
 scope.normalizePath = path.normalize;
 scope.getRelativePath = path.relative;
 scope.getAbsolutePath = path.resolce;
 scope.pathNoExt = function(s){return s.replace(/\.\w+$/, '')}
 
-function parseFile(file){
-	var source = fs.readFileSync(file, 'utf-8')
-	source = source.replace(/\r\n/g, '\n');
-	var ast = parse('\n\n' + source + '\n\n');
-	return ast;
-};
-
 var workingScope = fork(scope);
 workingScope.workingScope = workingScope;
 var cwd = path.dirname(scope.inputPath);
 do {
-	if(path.existsSync(path.join(cwd, '.common/default.ed'))){
-		workingScope.__transform(parseFile(path.join(cwd, '.common/default.ed')));
+	if(path.existsSync(path.resolve(cwd, '.common/default.ed'))){
+		workingScope.input(path.resolve(cwd, '.common/default.ed'));
 		scope.cwd = cwd;
 		break;
-	}
-} while(cwd !== (cwd = path.join(cwd, '..')));
-workingScope.__transform(parseFile(process.argv[2]));
+	};
+} while(cwd !== (cwd = path.resolve(cwd, '..')));
+workingScope.input(process.argv[2]);
